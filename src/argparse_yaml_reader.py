f173b97ad977142c46b3820f0158ef5b81629866
ericsalesdeandrade <sdaeric19@gmail.com>
2024-02-06 09:24:04 +0000

    Slight modifications

--- src/argparse_yaml_reader.py
+++ src/argparse_yaml_reader.py
@@ -1,25 +1,54 @@
 import yaml
-from argparse import ArgumentParser
+from typing import Dict
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-def arguments(location: list[str] | None):
+
+def parse_args(args=None) -> ArgumentParser.parse_args:
     """
-    Function to get user inputs
+    Function to parse command line arguments
+
+    Args:
+    args: list of strings to parse
+
+    Returns:
+    parsed_args: parsed arguments
     """
-    parser = ArgumentParser()
+    argument_parser = ArgumentParser(
+        description="Command line arguments for reading a configuration file",
+        formatter_class=ArgumentDefaultsHelpFormatter,
+    )
+    argument_parser.add_argument(
+        "--configpath", type=str, help="Configuration file path"
+    )
+    return argument_parser.parse_args(args)
 
-    parser.add_argument("location", help="Location to YAML file")
-    args = parser.parse_args(location)
-    yaml_reader(args.location)
 
-def yaml_reader(loc: str):
+def yaml_reader(path: str) -> Dict:
     """
     Function to read YAML config file
+
+    Args:
+    path: path to the YAML file
+
+    Returns:
+    data: dictionary of data from the YAML file
     """
-    with open(loc, "r") as yamlfile:
-     data = yaml.load(yamlfile, Loader=yaml.FullLoader)
-    print(data)
+    try:
+        with open(path, "r") as yamlfile:
+            data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+            return data
+    except Exception as e:
+        print(f"Error reading YAML file: {e}")
 
 
-## Here arguments() is the main function
+def main() -> None:
+    """
+    Main function to read YAML file
+    """
+    args = parse_args()
+    configpath = args.configpath
+    print(yaml_reader(path=configpath))
+
+
 if __name__ == "__main__":
-    arguments()
+    main()
--- src/typer_yaml_reader.py
+++ src/typer_yaml_reader.py
@@ -3,21 +3,32 @@ import typer
 
 app = typer.Typer()
 
-@app.command()
-def user_input(location:str):
-    """
-    Function to get user inputs
-    """
-    yaml_reader(location)
 
-def yaml_reader(loc: str):
+@app.command()
+def main(configpath: str) -> None:
+    """
+    Main function to read YAML file
+
+    Args:
+    configpath: path to the YAML file
+
+    Returns:
+    None
+    """
+    yaml_reader(configpath)
+
+
+def yaml_reader(path: str) -> None:
     """
     Function to read YAML config file
     """
-    with open(loc, "r") as yamlfile:
-     data = yaml.load(yamlfile, Loader=yaml.FullLoader)
-    print(data)
+    try:
+        with open(path, "r") as yamlfile:
+            data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+            print(data)
+    except Exception as e:
+        print(f"Error reading YAML file: {e}")
 
-## Here app() is the main function
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app()
